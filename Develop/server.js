e741ece66179cff7a1bd32d3ee5042f4c120bcbe
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-02-21 18:10:03 -0800

    added middleware to render my static html and added my routes with a comment showing my route logic with different syntax for readability

--- Develop/server.js
+++ Develop/server.js
@@ -10,18 +10,20 @@ const PORT = process.env.PORT || 3000;
 //Set up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 /***********************************
 Same thing as:
 
-const htmlRoutes = require('../routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 htmlRoutes(app);
 
 *************************************/
-//require('./routes/apiRoutes')(app);
+
 require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 
 
 //listener
-app.listen(PORT, () => console.log('App listening on PORT: ' + PORT));
+app.listen(PORT, () => console.log('App listening on PORT: ' + PORT + " Vist http://localhost:3000/"));
